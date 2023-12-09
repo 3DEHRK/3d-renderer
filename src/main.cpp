003e0cf62e3adb376d4b0db5a9dd36c4c3384672
laz <laurin.zoss@gitlab.ch>
2023-12-09 18:52:46 +0100

    3d space structures and cube mesh

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,30 @@
 #include <SFML/Graphics.hpp>
 
+struct vector3d{
+    float x, y, z;
+};
+
+struct triangle{
+    vector3d vertices[3];
+};
+
+struct mesh{
+    std::vector<triangle> triangles;
+
+    // Magic ChatGPT product, research in bjarnes book
+    // Overloading the assignment operator for mesh
+    mesh& operator=(const std::initializer_list<triangle>& init_list) {
+        triangles = init_list; // Assign the initializer list to the triangles vector
+        return *this;
+    }
+};
+
+mesh cube = {
+    //south
+    {0.f,0.f,0.f, 1.f,0.f,0.f, 1.f,1.f,0.f},
+    
+};
+
 int main() {
     sf::RenderWindow window(sf::VideoMode(800, 600), "3d renderer");
 
